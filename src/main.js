333c714b84cb74ed5eebc90cc59f578c83821ea3
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-05-28 23:21:57 +0300

    HW11-1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+import '/css/styles.css'
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
@@ -8,52 +8,73 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 const refs = {
-    formEl: document.querySelector('.search-word'),
-    searchEl: document.querySelector('.search-btn'),
+  formEl: document.querySelector('#search-form'),
+  searchEl: document.querySelector('.search-btn'),
 }
+
+refs.searchEl.addEventListener('click', () => {
+  const imageValue = refs.formEl.elements.input.value;
+
+  searchImage(imageValue)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.hits.length > 0) {
+                        imagesList(data.hits);
+                    } else {
+                        iziToast.error({
+                            position: 'topRight',
+                            message: 'Sorry, there are no images matching your search query. Please, try again!',
+                        });
+                    }
+                    refs.formEl.reset();
+                })
+                .catch(error => {
+                    console.error('Error fetching images:', error);
+                    iziToast.error({
+                        title: 'Error',
+                        message: 'Something went wrong. Please try again later.',
+                    });
+                });
+        });
 
 function searchImage(image) {
-    const BASE_URL = 'https://pixabay.com/api/';
-   
-    const params = new URLSearchParams({
-        key: '44102450-6df98fde061003ef9a12efa89',
-        q: image,
-        image_type: photo,
-        orientation: horizontal,
-        safesearch: true,
-    });
-    const url = `${BASE_URL}?${params}`;
+  const BASE_URL = 'https://pixabay.com/api/';
 
-    const headers = {
-        
-    };
+  const params = new URLSearchParams({
+    key: '44102450-6df98fde061003ef9a12efa89',
+    q: image,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  const url = `${BASE_URL}?${params}`;
 
-    return fetch(url, { headers }.then(res => res.json()));
+  return fetch(url);
 }
 
-searchImage('flower');
-
-
 function imagesList(images) {
-    return `<li>
-  <img src="" alt="">
-  <ul>
-    <li>
-      <h1></h1>
-      <p></p>
-    </li>
-    <li>
-      <h1></h1>
-      <p></p>
-    </li>
-    <li>
-      <h1></h1>
-      <p></p>
-    </li>
-    <li>
-      <h1></h1>
-      <p></p>
-    </li>
-  </ul>
-</li>`
-};
+  const gallery = document.getElementById('gallery');
+  
+  const imageMarkup = images
+    .map(image => 
+      `<a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags}">
+          <img src="${image.webformatURL}" alt="${image.tags}">
+          <ul class="image-info">
+              <li class="info-item">Likes<br><span>${image.likes}</span></д>
+              <li class="info-item">Views<br><span>${image.views}</span></д>
+              <li class="info-item">Comments<br><span>${image.comments}</span></д>
+              <li class="info-item">Downloads<br><span>${image.downloads}</span></д>
+          </ul>
+      </a>`
+    )
+    .join('');
+
+  gallery.innerHTML = imageMarkup;
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250 
+
+});
+}
